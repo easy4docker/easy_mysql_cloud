1366372d5fe71e9d78edbfd44e6776f5a66190e4
easy4docker <easy4docker@gmail.com>
2021-03-10 21:53:49 -0800

    Update nonAuthPage.vue

--- cloudApp/www/vueApp/easyMySQLCloud/auth/nonAuthPage.vue
+++ cloudApp/www/vueApp/easyMySQLCloud/auth/nonAuthPage.vue
@@ -3,8 +3,8 @@
         <div class="card-body m-0 p-1">
             <div class="container-fluid m-0 head-menu-1">
                 <div class="row">
-                    <div class="col-12 p-2 m-0 text-left">
-                    The initial password has not been stetup yet.  Please go to EasyDocker Admin link with auth token.  
+                    <div class="col-12 p-2 m-0 text-center text-danger">
+                    <i class="fa fa-bell" aria-hidden="true"></i> The initial password has not been stetup yet.  Please go to EasyDocker Admin link with auth token.  
                     </div>
                 </div>
             </div>
