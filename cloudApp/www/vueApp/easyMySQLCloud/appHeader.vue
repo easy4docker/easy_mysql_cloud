9dcf40410cbc6d85546fb7cd33bbe0f7ee950a72
easy4docker <easy4docker@gmail.com>
2021-03-10 16:06:11 -0800

    Update appHeader.vue

--- cloudApp/www/vueApp/easyMySQLCloud/appHeader.vue
+++ cloudApp/www/vueApp/easyMySQLCloud/appHeader.vue
@@ -12,7 +12,7 @@
                     </div>
                     <div class="col-2 p-0 m-0 text-right text-warning">
                             <a class="btn btn-danger border border-warning pull-right" 
-                                href="JavaScript:void(0)" v-on:click="rebootServer()">
+                                href="JavaScript:void(0)">
                             <i class="fa fa-power-off" aria-hidden="true"></i> Restart Server
                         </a>
                     </div>
@@ -22,33 +22,11 @@
                 <div class="row" v-if="root.isSignin()">
                     <div class="col-12 p-0 m-0 text-center">
                         <div class="pull-left border border-light rounded menu-frame shadow-sm">
-                            <button class="btn btn-outline-light m-1" 
-                                :disabled = "isDisabled('gridSetup')"
-                                v-if="!root.isLocalhost()"
-                                v-on:click="clickMenu('gridSetup')">
-                                <i class="fa fa-cogs" aria-hidden="true"></i> Grid Setup
-                            </button>
-                            <button class="btn btn-outline-light m-1" 
-                                :disabled = "isDisabled('form')"
-                                v-on:click="clickMenu('form')">
-                                <i class="fa fa-plus-square" aria-hidden="true"></i> Add a server
-                            </button>
-                            <button class="btn btn-outline-light m-1" 
-                                :disabled = "isDisabled('list')"
-                                v-on:click="clickMenu('list')">
-                                <i class="fa fa-tasks" aria-hidden="true"></i> List Servers
-                            </button>
+
                         </div>
 
                         <div class="m-0 text-center border border-light rounded menu-frame shadow-sm pull-right">
-                            <a class="btn btn-outline-light m-1" 
-                                href="JavaScript:void(0)" v-on:click="syncAppCode()">
-                                <i class="fa fa-github" aria-hidden="true"></i> Sync code
-                            </a>
-                            <a class="btn btn-outline-light m-1" 
-                                href="JavaScript:void(0)" v-on:click="root.signOff()">
-                                <i class="fa fa-sign-out" aria-hidden="true"></i> Sign Off
-                            </a>
+ 
                         </div>
                     </div>
                 </div>
@@ -66,36 +44,11 @@ module.exports = {
         }
     },
     mounted() {
-        this.gridSvrs = this.root.gridSvrs;
     },
     watch : {
 
     },
     methods :{
-        isDisabled(v) {
-            return (this.$parent.module === v);
-        },
-        clickMenu(v) {
-            var me = this;
-            me.$parent.module = v;
-        },
-        syncAppCode() {
-            var me = this;
-            me.root.dataEngine().appPost({
-                cmd : 'syncAppCode'
-            }, function(result) {
-                console.log(result);
-                window.location.reload();
-            }, true);
-        },
-        rebootServer() {
-            var me = this;
-            me.root.dataEngine().appPost({
-                cmd : 'rebootServer'
-            }, function(result) {
-                console.log(result);
-            }, true);
-        }
     }
 }
 </script>
