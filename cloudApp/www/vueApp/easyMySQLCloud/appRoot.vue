2d2448658be67cb4a40c93f45bc6ecf3dad9bd36
easy4docker <easy4docker@gmail.com>
2021-03-10 16:10:47 -0800

    update

--- cloudApp/www/vueApp/easyMySQLCloud/appRoot.vue
+++ cloudApp/www/vueApp/easyMySQLCloud/appRoot.vue
@@ -3,9 +3,7 @@
         <div class="row">
             <div class="col-1 p-0"></div>
             <div class="col-10 p-0 card text-center shadow border rounded">
-                header
                 <app-header></app-header>
-                header2
             </div>
             <div class="col-1 p-0"></div>
         </div>
