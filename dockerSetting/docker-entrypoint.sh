9fecc698279b82560377d479f174b5d1260943bc
easy4docker <76085700+easy4docker@users.noreply.github.com>
2021-03-12 15:29:38 -0800

    Update docker-entrypoint.sh

--- dockerSetting/docker-entrypoint.sh
+++ dockerSetting/docker-entrypoint.sh
@@ -4,6 +4,7 @@
 crond
 echo "@reboot root (echo _EASY_MYSQL && cd /var/_localApp/cloudApp && node server.js)" >> /etc/crontab
 echo "* * * * * root (echo _EASY_MIN_CRON && echo '$(date +"%m/%d %H:%M:%S")'  >> /tmp/minCron.txt)" >> /etc/crontab
+cd /var/_localApp/cloudApp && node server.js &
 # --- nodejs server related script --- end --
 
 # Copyright (c) 2017, Oracle and/or its affiliates. All rights reserved.
