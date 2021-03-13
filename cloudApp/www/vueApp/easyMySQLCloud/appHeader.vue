54b7ab38f9332976f376b2e97de70bf4e18d4e6f
easy4docker <easy4docker@gmail.com>
2021-03-13 00:45:10 -0800

    Update appHeader.vue

--- cloudApp/www/vueApp/easyMySQLCloud/appHeader.vue
+++ cloudApp/www/vueApp/easyMySQLCloud/appHeader.vue
@@ -1,19 +1,22 @@
 <template>
     <div class="head-card card m-1">
         <div class="card-body m-0 p-1 alert-info header-bg ">
+            
             <div class="container-fluid m-0 head-menu-1">
                 <div class="row">
                     <div class="col-1 p-0 m-0 text-left">
                     </div>
                     <div class="col-9 p-3 text-center">
+                        
                         <h1 class="header-title m-3 text-light">Easy MySQL Cloud 
                             <span class="version">(Version &alpha;)</span></h1>
                     </div>
-                    <div class="col-2 p-0 m-0 text-right text-warning">
+                    <div class="col-2 pr-1 m-0 text-right text-warning">
                     
                     </div>
                 </div>
             </div>
+            <div class="pull-right pl-1 text-warning">Developer : John Xu</div>
             <div class="container-fluid mt-1 head-menu-2 text-left text-light">
                 <a href="JavaScript:void(0)" v-if="root.module !== 'sql'"  v-on:click="module('sql')" class="m-3">SQL Tool</a>
                 <b class="m-3" v-if="root.module === 'sql'">SQL Tool</b> |
@@ -22,6 +25,7 @@
                 <a href="JavaScript:void(0)" v-if="root.module !== 'documents'"  v-on:click="module('documents')" class="m-3">Documents</a>
                 <b class="m-3" v-if="root.module === 'documents'">Documents</b>
             </div>
+            
         </div>
     </div> 
 </template>
